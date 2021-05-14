e7800dff200f575e2193fa53deefb116682819d0
hongjeongseob <hongjeongseob82@gmail.com>
2021-05-15 01:05:06 +0900

    Added news search function

--- NewsApp/NewsApp/Utils/APICaller.swift
+++ NewsApp/NewsApp/Utils/APICaller.swift
@@ -12,6 +12,7 @@ final class APICaller {
     
     struct Constants {
         static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/everything?q=tesla&from=2021-04-14&sortBy=publishedAt&apiKey=63a700b5a752493bb6665bf99dda23bc")
+        static let searchUrlString = "https://newsapi.org/v2/everything?from=2021-04-14&sortBy=publishedAt&apiKey=63a700b5a752493bb6665bf99dda23bc&q="
     }
     
     private init() {}
@@ -19,6 +20,18 @@ final class APICaller {
     public func getTopStories(completion: @escaping (Result<[Article], Error>) -> Void) {
         guard let url = Constants.topHeadlinesURL else { return }
         
+        fetchData(byUrl: url, completion: completion)
+    }
+    
+    public func search(with query: String, completion: @escaping (Result<[Article], Error>) -> Void) {
+        guard !query.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+        let urlString = Constants.searchUrlString + query
+        guard let url = URL(string: urlString) else { return }
+        
+        fetchData(byUrl: url, completion: completion)
+    }
+    
+    private func fetchData(byUrl url: URL, completion: @escaping (Result<[Article], Error>) -> Void) {
         let task = URLSession.shared.dataTask(with: url) { data, _, error in
             if let error = error {
                 completion(.failure(error))
@@ -27,14 +40,15 @@ final class APICaller {
                 do {
                     let result = try JSONDecoder().decode(APIResponse.self, from: data)
                     completion(.success(result.articles))
-                    
+
                 } catch {
                     completion(.failure(error))
                 }
-                
+
             }
         }
         task.resume()
     }
+
 }
 
