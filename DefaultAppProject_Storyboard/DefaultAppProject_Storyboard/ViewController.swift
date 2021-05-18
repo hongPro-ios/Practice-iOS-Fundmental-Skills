16f69ae76df5c33d70a2eff4f9c71036d32fa86c
hongjeongseob <hongjeongseob82@gmail.com>
2021-05-18 21:48:47 +0900

    Add Default UIKit project

---
+++ DefaultAppProject_Storyboard/DefaultAppProject_Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DefaultAppProject_Storyboard
+//
+//  Created by JEONGSEOB HONG on 2021/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
