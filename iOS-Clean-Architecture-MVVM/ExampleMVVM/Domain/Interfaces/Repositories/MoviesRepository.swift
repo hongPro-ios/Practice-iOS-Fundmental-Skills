18143da9aae8d15f345faae118b3269516873a1b
hongjeongseob <hongjeongseob82@gmail.com>
2021-05-18 21:47:30 +0900

    Edit code style

--- iOS-Clean-Architecture-MVVM/ExampleMVVM/Domain/Interfaces/Repositories/MoviesRepository.swift
+++ iOS-Clean-Architecture-MVVM/ExampleMVVM/Domain/Interfaces/Repositories/MoviesRepository.swift
@@ -9,7 +9,10 @@ import Foundation
 
 protocol MoviesRepository {
     @discardableResult
-    func fetchMoviesList(query: MovieQuery, page: Int,
-                         cached: @escaping (MoviesPage) -> Void,
-                         completion: @escaping (Result<MoviesPage, Error>) -> Void) -> Cancellable?
+    func fetchMoviesList(
+        query: MovieQuery,
+        page: Int,
+        cached: @escaping (MoviesPage) -> Void,
+        completion: @escaping (Result<MoviesPage, Error>) -> Void
+    ) -> Cancellable?
 }
