1b98c168114ec8c021e3de07c67b529fe1d4ddc8
hongPro-ios <hongjeongseob@icloud.com>
2021-06-18 14:43:42 +0900

    Add Pinch Gesture setting practice

--- GestureRecognizers/GestureRecognizers/Pinch/PinchGestureViewController.swift
+++ GestureRecognizers/GestureRecognizers/Pinch/PinchGestureViewController.swift
@@ -23,13 +23,19 @@
 import UIKit
 
 class PinchGestureViewController: UIViewController {
-   
-   @IBOutlet weak var imageView: UIImageView!
-   
-   
-   
-   @IBAction func reset(_ sender: Any) {
-      
-   }
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func handlePinch(_ sender: UIPinchGestureRecognizer) {
+        guard let targetView = sender.view else { return }
+        
+        targetView.transform = targetView.transform.scaledBy(x: sender.scale, y: sender.scale)
+        sender.scale = 1
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        UIView.animate(withDuration: 0.3) {
+            self.imageView.transform = CGAffineTransform.identity
+        }
+    }
+    
 }
